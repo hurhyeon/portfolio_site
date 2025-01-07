faea4a5a61ba95eafb7da639c0113b82be314dd9
hurhyeon <90999300+hurhyeon@users.noreply.github.com>
2025-01-08 00:19:56 +0900

    Update Navbar.js

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Link from 'next/link'
 
+
 export const Nav = ({ title, links }) => {
 
   const [isNavCollapsed, setIsNavCollapsed] = useState(true);
